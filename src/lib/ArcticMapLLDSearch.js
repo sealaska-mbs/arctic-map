68dc2954e33574e80fd73d742134625acd8cda39
Deborah Lamb <deborah.lamb@mbshome.com>
2021-02-01 09:59:27 -0700

    Sprint 6

--- src/lib/ArcticMapLLDSearch.js
+++ src/lib/ArcticMapLLDSearch.js
@@ -78,10 +78,247 @@ static displayName = 'ArcticMapLLDSearch';
                    
                     getSuggestions: function (params) {
   
-                        var serarcParams = params.suggestTerm.replace(/\+/g,' ');
+                        var searchParams = params.suggestTerm.replace(/\+/g,' ');
+//geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Foot&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnExtentOnly=false&featureEncoding=esriDefault
+                        var searchParts = searchParams.Trim().replace(/\s+/g, ' ').split(' ');
+
+                        if(searchParts.length>4){
+                            var options = {
+                                query :{
+                                    "legaldescription": searchParams,
+                                    "returnalllevels": "true",
+                                    f:"pjson"
+                                },
+                                responseType: "json"
+                            }
+                            return esriRequest(url, options).then(function(results) {
+                           
+                                return results.data.features.map(function (feature) {
+                                    return {
+                                        key: "name",
+                                        text: feature.attributes.landdescription,
+                                        sourceIndex: params.sourceIndex
+                                    };
+                                });
+                            });                            
+                        }
+
+                        var PLSSurl = "https://gis.blm.gov/arcgis/rest/services/Cadastral/BLM_Natl_PLSS_CadNSDI/MapServer/1/query";
+                        //get the states
+                        if(!self.suggestionDictionary){
+                            self.suggestionDictionary = {};
+
+                            var queryParams = {
+                                query :{
+                                    where: "STATEABBR IS NOT NULL",
+                                    outFields: "STATEABBR",
+                                    returnGeometry: "false",
+                                    returnDistinctValues: "true",
+                                    f: "pjson"
+                                },
+                                responseType: "json"
+                            };
+                            esriRequest(PLSSurl, queryParams).then(function(results) {
+                                results.data.features.map(function (feature) {
+                                    self.suggestionDictionary[feature.attributes["STATEABBR"]] = null;
+                                });
+                            });
+                        }
+                        if(searchParts===null || searchParts.length===0){
+                            return suggestionDictionary.map(function(state){
+                                return {
+                                    key: "name",
+                                    text: state + " ",
+                                    sourceIndex: params.sourceIndex
+                                };
+
+                            });
+                        }
+                        var states = suggestionDictionary.map(function(state){
+                            if(state.indexOf(searchParts[0])>=0){
+                                return state;
+                            }
+                        });
+                        if(states.length<1 || (states.length>1 && searchParts.length>1)){
+                            return null;
+                        }
+                        if(states.length>1){
+                            return states.map(function(state){
+                                return {
+                                    key: "name",
+                                    text: state + " ",
+                                    sourceIndex: params.sourceIndex
+                                };
+
+                            });
+                        }
+                        //only 1 state get meridians
+                        if(!self.suggestionDictionary[states[0]]){
+                            self.suggestionDictionary[states[0]] = {};
+
+                            var queryParams = {
+                                query :{
+                                    where: "STATEABBR='"+states[0]+"' AND PRINMERCD IS NOT NULL",
+                                    outFields: "PRINMERCD",
+                                    returnGeometry: "false",
+                                    returnDistinctValues: "true",
+                                    f: "pjson"
+                                },
+                                responseType: "json"
+                            };
+                            esriRequest(PLSSurl, queryParams).then(function(results) {
+                                results.data.features.map(function (feature) {
+                                    self.suggestionDictionary[states[0]][feature.attributes["PRINMERCD"]] = null;
+                                });
+                            });
+                        }
+                        var meridianDict = self.suggestionDictionary[states[0]];
+                        if(searchParts.length===1 && meridianDict.length>1){
+                            return meridianDict.map(function(meridian){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridian + " T",
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+                        var meridians = meridianDict.map(function(meridian){
+                             return meridian;
+                        });    
+                        if(searchParts.length>1){
+                            meridians = meridianDict.map(function(meridian){
+                                if(meridian.indexOf(searchParts[1])>=0){
+                                    return meridian;
+                                }
+                            });    
+                        }
+                        if(meridians.length<1){
+                            return null;
+                        }
+                        if(meridians.length>1){
+                            return meridians.map(function(meridian){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridian + " T",
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+                        //only 1 meridian get tracts
+                        if(!meridianDict[meridians[0]]){
+                            meridianDict[meridians[0]] = {};
+
+                            var queryParams = {
+                                query :{
+                                    where: "STATEABBR='"+states[0]+"' AND PRINMERCD='"+meridians[0]+"' AND TWNSHPNO IS NOT NULL AND (TWNSHPDIR='N' OR TWNSHPDIR='S')",
+                                    outFields: "TWNSHPNO, TWNSHPDIR",
+                                    returnGeometry: "false",
+                                    returnDistinctValues: "true",
+                                    f: "pjson"
+                                },
+                                responseType: "json"
+                            };
+                            esriRequest(PLSSurl, queryParams).then(function(results) {
+                                results.data.features.map(function (feature) {
+                                    var twp = "T" + feature.attributes["TWNSHPNO"].TrimLeft('0') + feature.attributes["TWNSHPDIR"];
+                                    meridianDict[meridians[0]][twp] = null;
+                                });
+                            });
+                        }
+                        var townshipDict = meridianDict[meridians[0]];
+                        if(searchParts.length===2 && townshipDict.length>1){
+                            return townshipDict.map(function(township){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridians[0] + " " + township,
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+                        var townships = townshipDict.map(function(township){
+                             return township;
+                        });    
+                        if(searchParts.length>2){
+                            townships = townshipDict.map(function(township){
+                                if(township.indexOf(searchParts[2])>=0){
+                                    return township;
+                                }
+                            });    
+                        }
+                        if(townships.length<1){
+                            return null;
+                        }
+                        if(townships.length>1){
+                            return townships.map(function(township){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridians[0] + " " + township,
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+
+                        //only 1 township get ranges
+                        if(!townshipDict[townships[0]]){
+                            townshipDict[townships[0]] = {};
+
+                            var twpno = townships[0].substring(1, townships[0].length-1);
+                            var twpdir = townships[0].substring(townships[0].length-1, townships[0].length)
+
+                            var queryParams = {
+                                query :{
+                                    where: "STATEABBR='"+states[0]+"' AND PRINMERCD='"+meridians[0]+"' AND (TWNSHPNO='"+twpno+"' OR TWNSHPNO='"+twpno.padStart(3,0)+"') AND TWNSHPDIR='"+twpdir+"' AND RANGENO IS NOT NULL AND (RANGEDIR='E' OR RANGEDIR='W')",
+                                    outFields: "RANGENO, RANGEDIR",
+                                    returnGeometry: "false",
+                                    returnDistinctValues: "true",
+                                    f: "pjson"
+                                },
+                                responseType: "json"
+                            };
+                            esriRequest(PLSSurl, queryParams).then(function(results) {
+                                results.data.features.map(function (feature) {
+                                    var rng = "R" + feature.attributes["RANGENO"].TrimLeft('0') + feature.attributes["RANGEDIR"];
+                                    townshipDict[townships[0]][rng] = null;
+                                });
+                            });
+                        }
+                        var rangeDict = townshipDict[townships[0]];
+                        if(searchParts.length===3 && rangeDict.length>1){
+                            return rangeDict.map(function(range){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridians[0] + " " + townships[0] + " " + range,
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+                        var ranges = rangeDict.map(function(range){
+                             return range;
+                        });    
+                        if(searchParts.length>3){
+                            ranges = rangeDict.map(function(range){
+                                if(range.indexOf(searchParts[3])>=0){
+                                    return range;
+                                }
+                            });    
+                        }
+                        if(ranges.length<1){
+                            return null;
+                        }
+                        if(ranges.length>1){
+                            return ranges.map(function(range){
+                                return {
+                                    key: "name",
+                                    text: states[0] + " " + meridians[0] + " " + townships[0] + " " + range,
+                                    sourceIndex: params.sourceIndex
+                                };
+                            });
+                        }
+
+                        //only 1 range
                         var options = {
                             query :{
-                                "legaldescription": serarcParams,
+                                "legaldescription": states[0] + " " + meridians[0] + " " + townships[0] + " " + ranges[0],
                                 "returnalllevels": "true",
                                 f:"pjson"
                             },
