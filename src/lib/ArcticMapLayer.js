008950df8f7fac5208ceacaa5ca990191a138413
daniellongmatey <daniel.longmatey@mbshome.com>
2021-06-22 17:38:21 -0400

    Added a displayTitle prop to renderer, which can be used to override a sublayer's title

--- src/lib/ArcticMapLayer.js
+++ src/lib/ArcticMapLayer.js
@@ -280,6 +280,9 @@ class ArcticMapLayer extends React.Component {
                         if (renderer !== undefined) {
                             //console.log("Sublayer renderer:", renderer.props.style);
                             sublayer.renderer = renderer.props.style;
+                            if (renderer.props.displayTitle !== undefined) {
+                                sublayer.title = renderer.props.displayTitle;
+                            }
                         }
                         //sublayer.renderer = Renderer.fromJSON(renderer);
 
